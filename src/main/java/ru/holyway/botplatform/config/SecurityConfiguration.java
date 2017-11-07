72e63f00dac44b99fefa5ff9ecae85868c6727e0
Sergei I. Ivanov <sergei.i.ivanov@netcracker.com>
2017-11-07 11:40:22 +0400

    disable authorization

--- src/main/java/ru/holyway/botplatform/config/SecurityConfiguration.java
+++ src/main/java/ru/holyway/botplatform/config/SecurityConfiguration.java
@@ -1,16 +1,29 @@
 package ru.holyway.botplatform.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityConfigurer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by seiv0814 on 07-11-17.
  */
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration extends
+        WebSecurityConfigurerAdapter {
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**"); // #3
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .anyRequest().
+                permitAll();
+    }
 }
