d4e61ae0894e526ff3b9e5d715abe5466caa5d4f
Sergei I. Ivanov <sergei.i.ivanov@netcracker.com>
2018-02-05 17:27:04 +0400

    fix for secret chats

--- src/main/java/ru/holyway/botplatform/telegram/processor/SecretMessageProcessor.java
+++ src/main/java/ru/holyway/botplatform/telegram/processor/SecretMessageProcessor.java
@@ -2,8 +2,10 @@ package ru.holyway.botplatform.telegram.processor;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.api.objects.CallbackQuery;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import ru.holyway.botplatform.telegram.TelegramMessageEntity;
@@ -31,18 +33,25 @@ public class SecretMessageProcessor implements MessageProcessor {
     public void process(TelegramMessageEntity messageEntity) throws TelegramApiException {
         List<Integer> messages = secretMessages.get(messageEntity.getChatId());
         if (messages != null) {
-            if (StringUtils.isNotEmpty(messageEntity.getText()) && StringUtils.equalsIgnoreCase(messageEntity.getText(), "/secret")) {
+            if (StringUtils.isNotEmpty(messageEntity.getText()) && StringUtils.containsIgnoreCase(messageEntity.getText(), "/secret")) {
                 messages.add(messageEntity.getMessage().getMessageId());
                 for (Integer integer : messages) {
-                    messageEntity.getSender().execute(new DeleteMessage().setChatId(messageEntity.getChatId()).setMessageId(integer));
+                    try {
+                        messageEntity.getSender().execute(new DeleteMessage().setChatId(messageEntity.getChatId()).setMessageId(integer));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    secretMessages.remove(messageEntity.getChatId());
                 }
             } else {
                 messages.add(messageEntity.getMessage().getMessageId());
             }
         } else {
-            if (StringUtils.isNotEmpty(messageEntity.getText()) && StringUtils.equalsIgnoreCase(messageEntity.getText(), "/secret")) {
+            if (StringUtils.isNotEmpty(messageEntity.getText()) && StringUtils.containsIgnoreCase(messageEntity.getText(), "/secret")) {
                 messages = new ArrayList<>();
                 messages.add(messageEntity.getMessage().getMessageId());
+                Message message = messageEntity.getSender().execute(new SendMessage().setChatId(messageEntity.getChatId()).setText("Ах вы шалушники, так и придётся скрыть ваши разговоры."));
+                messages.add(message.getMessageId());
                 secretMessages.put(messageEntity.getChatId(), messages);
             }
         }
