c74a9086c7a8342086049c0cde96d3452eabee80
mrMigles <delta_001@bk.ru>
2018-04-28 01:22:20 +0400

    badumtsss

--- src/main/java/ru/holyway/botplatform/telegram/processor/BaDumTssMessageProcessor.java
+++ src/main/java/ru/holyway/botplatform/telegram/processor/BaDumTssMessageProcessor.java
@@ -20,20 +20,24 @@ public class BaDumTssMessageProcessor implements MessageProcessor {
 
     @Override
     public boolean isNeedToHandle(TelegramMessageEntity messageEntity) {
-        return StringUtils.isNotEmpty(messageEntity.getMessage().getText());
+        if (StringUtils.isNotEmpty(messageEntity.getMessage().getText())) {
+            final String mes = messageEntity.getText();
+            if (isUpper(mes)) {
+                if (last.get(messageEntity.getChatId()) != null && messageEntity.getMessage().getFrom().getId().equals(last.get(messageEntity.getChatId()))) {
+                    return true;
+                }
+            } else {
+                last.put(messageEntity.getChatId(), messageEntity.getMessage().getFrom().getId());
+            }
+        }
+        return false;
     }
 
     @Override
     public void process(TelegramMessageEntity messageEntity) throws TelegramApiException {
-        final String mes = messageEntity.getText();
-        if (isUpper(mes)) {
-            if (last.get(messageEntity.getChatId()) != null && messageEntity.getMessage().getFrom().getId().equals(last.get(messageEntity.getChatId()))) {
-                messageEntity.getSender().execute(new SendMessage().setChatId(messageEntity.getChatId()).setText("https://cs8.pikabu.ru/images/previews_comm/2017-09_1/1504277920155595784.png"));
-                last.remove(messageEntity.getChatId());
-            }
-        } else {
-            last.put(messageEntity.getChatId(), messageEntity.getMessage().getFrom().getId());
-        }
+        messageEntity.getSender().execute(new SendMessage().setChatId(messageEntity.getChatId()).setText("https://cs8.pikabu.ru/images/previews_comm/2017-09_1/1504277920155595784.png"));
+        last.remove(messageEntity.getChatId());
+
     }
 
     @Override
