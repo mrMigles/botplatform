b00361fb6b63f948c343fa37d59a0052a8417552
mrMigles <delta_001@bk.ru>
2018-08-07 22:34:58 +0400

    Remove job

--- src/main/java/ru/holyway/botplatform/BotPlatformApplication.java
+++ src/main/java/ru/holyway/botplatform/BotPlatformApplication.java
@@ -1,16 +1,13 @@
 package ru.holyway.botplatform;
 
-import java.util.List;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.telegram.telegrambots.ApiContextInitializer;
-import ru.holyway.botplatform.config.JobInitializer;
 import ru.holyway.botplatform.core.Bot;
 
 @EnableWebSecurity
--- src/main/java/ru/holyway/botplatform/core/CommonMessageHandler.java
+++ src/main/java/ru/holyway/botplatform/core/CommonMessageHandler.java
@@ -1,92 +1,90 @@
 package ru.holyway.botplatform.core;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import ru.holyway.botplatform.config.JobInitializer;
 import ru.holyway.botplatform.core.data.DataHelper;
 import ru.holyway.botplatform.core.handler.MessageHandler;
-
-import javax.annotation.PostConstruct;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Sergey on 1/17/2017.
  */
 public class CommonMessageHandler implements CommonHandler {
 
-    @Autowired
-    private DataHelper dataHelper;
+  @Autowired
+  private DataHelper dataHelper;
 
-    @Autowired
-    @Qualifier("orderedMessageHandlers")
-    private List<MessageHandler> messageHandlers;
+  @Autowired
+  @Qualifier("orderedMessageHandlers")
+  private List<MessageHandler> messageHandlers;
 
-    @Autowired
-    private Context context;
+  @Autowired
+  private Context context;
 
-    @Value("${bot.config.silentPeriod}")
-    private String silentPeriodString;
+  @Value("${bot.config.silentPeriod}")
+  private String silentPeriodString;
 
-    private long srartTime = 0;
-    private long silentPeriod = TimeUnit.SECONDS.toMillis(60);
+  private long srartTime = 0;
+  private long silentPeriod = TimeUnit.SECONDS.toMillis(60);
 
 
-    public CommonMessageHandler() {
+  public CommonMessageHandler() {
 
+  }
+
+  @PostConstruct
+  public void postConstruct() {
+    srartTime = System.currentTimeMillis();
+    if (StringUtils.isNotEmpty(silentPeriodString)) {
+      silentPeriod = TimeUnit.SECONDS.toMillis(Long.parseLong(silentPeriodString));
     }
+  }
 
-    @PostConstruct
-    public void postConstruct() {
-        srartTime = System.currentTimeMillis();
-        if (StringUtils.isNotEmpty(silentPeriodString)) {
-            silentPeriod = TimeUnit.SECONDS.toMillis(Long.parseLong(silentPeriodString));
+  @Override
+  public String generateAnswer(MessageEntity messageEntity) {
+    if (messageEntity != null) {
+
+      for (MessageHandler messageHandler : messageHandlers) {
+        try {
+          String message = messageHandler.provideAnswer(messageEntity);
+          if (message != null) {
+            return message;
+          }
+        } catch (ProcessStopException e) {
+          System.out.println("Stop because: " + e.getMessage());
+          break;
         }
+      }
     }
+    return null;
+  }
 
-    @Override
-    public String generateAnswer(MessageEntity messageEntity) {
-        if (messageEntity != null) {
-
-            for (MessageHandler messageHandler : messageHandlers) {
-                try {
-                    String message = messageHandler.provideAnswer(messageEntity);
-                    if (message != null) {
-                        return message;
-                    }
-                } catch (ProcessStopException e) {
-                    System.out.println("Stop because: " + e.getMessage());
-                    break;
-                }
-            }
-        }
-        return null;
+  @Override
+  public void handleMessage(MessageEntity messageEntity) {
+    final String answer = generateAnswer(messageEntity);
+    if (!StringUtils.isEmpty(answer)) {
+      sendMessage(messageEntity, answer);
     }
+  }
 
-    @Override
-    public void handleMessage(MessageEntity messageEntity) {
-        final String answer = generateAnswer(messageEntity);
-        if (!StringUtils.isEmpty(answer)) {
-            sendMessage(messageEntity, answer);
-        }
+  private void sendMessage(MessageEntity messageEntity, String text) {
+    long currentTime = System.currentTimeMillis();
+    if (currentTime - this.srartTime > silentPeriod) {
+      if (StringUtils.containsIgnoreCase(text, "Денис") || StringUtils
+          .containsIgnoreCase(messageEntity.getText(), "Денис")) {
+        context.incrementDenisCount();
+      }
+      sendMessageInternal(messageEntity, text);
+      context.setLastStamp(System.currentTimeMillis());
+      context.incrementCount();
     }
+  }
 
-    private void sendMessage(MessageEntity messageEntity, String text) {
-        long currentTime = System.currentTimeMillis();
-        if (currentTime - this.srartTime > silentPeriod) {
-            if (StringUtils.containsIgnoreCase(text, "Денис") || StringUtils.containsIgnoreCase(messageEntity.getText(), "Денис")) {
-                context.incrementDenisCount();
-            }
-            sendMessageInternal(messageEntity, text);
-            JobInitializer.ITER = 0;
-            context.setLastStamp(System.currentTimeMillis());
-            context.incrementCount();
-        }
-    }
-
-    protected void sendMessageInternal(MessageEntity messageEntity, String text) {
-        messageEntity.reply(text);
-    }
+  protected void sendMessageInternal(MessageEntity messageEntity, String text) {
+    messageEntity.reply(text);
+  }
 }
